37ea69cd1994d497dd80a554ac152d436ae35d7e
NikolajRFA <34486013+NikolajRFA@users.noreply.github.com>
2023-02-10 23:56:05 +0100

    Added support for adding multiple persons at once.

--- src/main.rs
+++ src/main.rs
@@ -9,51 +9,61 @@ fn main() {
     println!("\nExit the program by typing 'exit'");
 
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
 
     if input.trim().eq(&String::from("exit")) {
         process::exit(0);
     }
 
-    let name_age: Vec<&str> = input.trim().split(";").collect();
+    let name_age_sets: Vec<&str> = input.trim().split(",").collect();
 
-    let name = name_age[0].trim();
-    let age = name_age[1].trim();
-    
-    println!("Hello, {}!", name);
+    for set in name_age_sets {
+        let name_age: Vec<&str> = set.trim().split(";").collect();
 
-    let conn = Connection::open("name_database.db").unwrap();
+        let name = name_age[0].trim();
+        let age = name_age[1].trim();
 
-    if !table_exists("Persons", &conn) {
-        // Create Persons table.
-        conn.execute(
-            "CREATE TABLE Persons (
+        println!("Hello, {}!", name);
+
+        let conn = Connection::open("name_database.db").unwrap();
+
+        if !table_exists("Persons", &conn) {
+            // Create Persons table.
+            conn.execute(
+                "CREATE TABLE Persons (
                     id              INTEGER PRIMARY KEY AUTOINCREMENT,
                     name            TEXT NOT NULL,
                     age             INTEGER NULL
                     )",
-            params![],
-        ).unwrap();
+                params![],
+            )
+            .unwrap();
+        }
+
+        conn.execute(
+            "INSERT INTO Persons (name, age) VALUES (?1, ?2)",
+            params![name, age],
+        )
+        .unwrap();
+
+        let selected_name: String = conn
+            .query_row(
+                "SELECT name FROM Persons WHERE id = (SELECT MAX(id) FROM Persons)",
+                params![],
+                |row| row.get(0),
+            )
+            .unwrap();
+
+        println!("{} has been added!", selected_name);
     }
-
-    conn.execute(
-        "INSERT INTO Persons (name, age) VALUES (?1, ?2)",
-        params![name, age],
-    )
-    .unwrap();
-
-    let selected_name: String = conn
-        .query_row("SELECT name FROM Persons WHERE id = (SELECT MAX(id) FROM Persons)", params![], |row| {
-            row.get(0)
-        }).unwrap();
-
-    println!("{} has been added!", selected_name);
 }
 
 fn table_exists(name: &str, conn: &Connection) -> bool {
     let sql: &str = "SELECT count() count FROM sqlite_master WHERE name = ?";
-    
-    return conn.query_row(sql, params![name], |row| {
-        row.get(0)
-    }).unwrap();
+
+    return conn
+        .query_row(sql, params![name], |row| row.get(0))
+        .unwrap();
 }
